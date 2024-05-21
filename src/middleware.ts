0fa96028b8236209030413281af536e747a0c462
Abdelrahman mostafa <a.abdo.mae@gmail.com>
2024-05-22 00:06:02 +0300

    admin auth

---
+++ src/middleware.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from "next/server"
+
+export async function middleware(req: NextRequest) {
+  if ((await isAuthenticated(req)) === false) {
+    return new NextResponse("Unauthorized", {
+      status: 401,
+      headers: { "WWW-Authenticate": "Basic" },
+    })
+  }
+}
+
+async function isAuthenticated(req: NextRequest) {
+  const authHeader =
+    req.headers.get("authorization") || req.headers.get("Authorization")
+
+  if (authHeader == null) return false
+
+  const [username, password] = Buffer.from(authHeader.split(" ")[1], "base64")
+    .toString()
+    .split(":")
+
+  return (
+    username === 'admin' && password === 'admin'
+  )
+}
+
+export const config = {
+  matcher: "/admin/:path*",
+}
