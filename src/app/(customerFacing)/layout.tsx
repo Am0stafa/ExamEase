90bc3c5f3e2a6c5f46e905c25a25fd8f9d32c0bd
Abdelrahman mostafa <a.abdo.mae@gmail.com>
2024-05-23 17:19:07 +0300

    fix: auth

--- src/app/(customerFacing)/layout.tsx
+++ src/app/(customerFacing)/layout.tsx
@@ -1,24 +1,30 @@
 import { Nav, NavLink } from "@/components/Nav"
 import { auth } from "../../../auth"
-import { Button } from "@/components/ui/button"
-import Link from "next/link"; // Add Link import
-
-export const dynamic = "force-dynamic"
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Layout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <>
       <Nav>
-        <NavLink href="/">Home</NavLink>
-        <NavLink href="/products">Products</NavLink>
-        <NavLink href="/orders">My Orders</NavLink>
-        <NavLink href="/sign-in">Authenticate</NavLink>
+        <div className="flex justify-between items-center">
+          <div>
+            <NavLink href="/">Home</NavLink>
+            <NavLink href="/products">Products</NavLink>
+            <NavLink href="/orders">My Orders</NavLink>
+          </div>
+          <div>
+            <Link href="/sign-in">
+              <Button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Authenticate</Button>
+            </Link>
+          </div>
+        </div>
       </Nav>
       <div className="container my-6">{children}</div>
     </>
-  )
+  );
 }
