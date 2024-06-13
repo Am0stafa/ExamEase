0b9a9b853a83c37756c8f127f5c35bac4df74fcd
Abdelrahman mostafa <a.abdo.mae@gmail.com>
2024-06-13 15:30:16 +0300

    feat: checkout page

--- src/app/(customerFacing)/products/[id]/purchase/page.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 
 export default async function PurchasePage({
   params: { id },
-}: {
+}:{
   params: { id: string }
 }) {
   const product = await db.product.findUnique({ where: { id } })
